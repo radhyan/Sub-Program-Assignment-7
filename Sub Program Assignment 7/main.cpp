48d3f57c91f09e8cdaa057c4ea5a043f54b81675
Radhyanas <radhyanasoetomo@mail.ugm.ac.id>
2022-11-08 19:20:56 +0700

    Initial Commit

---
+++ Sub Program Assignment 7/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sub Program Assignment 7
+//
+//  Created by Radhyanas on 08/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
